0cd3f48b90ded4146a1306e465e9b4e43d502e97
pibic2018 <32969011+pibic2018@users.noreply.github.com>
2017-11-20 04:47:55 -0300

    Update config.php

--- config.php
+++ config.php
@@ -7,7 +7,7 @@
     - create a new App and retrieve the app ID and namespace from Facebook
     Developer site at https://developers.facebook.com/apps
 */
-  $serverUrl = "https://agrolandia.herokuapp.com/";
+  $serverUrl = "https://agrolandia.herokuapp.com";
   $appNamespace = "agrolandy";
   $appId = "162411127828792";
 ?>
